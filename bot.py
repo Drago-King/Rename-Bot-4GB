386f42aab1a9dbea78dc71d6c42008c033675907
Drago-King <106234416+Drago-King@users.noreply.github.com>
2026-03-15 01:26:32 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,33 +3,28 @@ from pyrogram import Client, idle
 from plugins.cb_data import app as Client2
 from config import *
 import pyrogram.utils
+import asyncio
 
 pyrogram.utils.MIN_CHAT_ID = -999999999999
 pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
-
-
 bot = Client("Renamer", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
 
 
+async def main():
+    from plugins.channel_rename import _queue_worker
+    asyncio.create_task(_queue_worker())
+
+    if STRING_SESSION:
+        await Client2.start()
+        await bot.start()
+        await idle()
+        await bot.stop()
+        await Client2.stop()
+    else:
+        await bot.start()
+        await idle()
+        await bot.stop()
 
 
-if STRING_SESSION:
-    apps = [Client2,bot]
-    for app in apps:
-        app.start()
-    idle()
-    for app in apps:
-        app.stop()
-    
-else:
-    bot.run()
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Back-Up Channel @JishuBotz
-# Developer @JishuDeveloper
+asyncio.run(main())
