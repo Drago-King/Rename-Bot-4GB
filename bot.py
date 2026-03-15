fc17029ac27a9de19f050a63de04a1a5784d4794
Drago-King <106234416+Drago-King@users.noreply.github.com>
2026-03-15 08:20:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ from pyrogram import Client, idle
 from plugins.cb_data import app as Client2
 from config import *
 import pyrogram.utils
-import asyncio
 
 pyrogram.utils.MIN_CHAT_ID = -999999999999
 pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
@@ -14,17 +13,8 @@ if STRING_SESSION:
     apps = [Client2, bot]
     for app in apps:
         app.start()
-    # Start queue worker after bot is running
-    loop = asyncio.get_event_loop()
-    from plugins.channel_rename import _queue_worker
-    loop.create_task(_queue_worker())
     idle()
     for app in apps:
         app.stop()
 else:
-    bot.start()
-    loop = asyncio.get_event_loop()
-    from plugins.channel_rename import _queue_worker
-    loop.create_task(_queue_worker())
-    idle()
-    bot.stop()
+    bot.run()
